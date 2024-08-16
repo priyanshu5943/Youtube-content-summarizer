71e1c9319cef1a4193e56b7f07ce634850becc2a
priyanshu shukla <105591596+priyanshu5943@users.noreply.github.com>
2024-08-16 18:34:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
-
+import youtube_transcript_api
 
 ## sstreamlit APP
 st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
